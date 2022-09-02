b5a68e3390759a48273a155cc5095ee9b388d6cb
Yury Zharikov <zharikovyuri@gmail.com>
2022-09-02 08:53:57 +0300

    first review5

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
 from django.contrib.auth import get_user_model
 from django.views.decorators.cache import cache_page
 
